ac6e2de6eddfd56418473466f783c0cd02148cad
RODRIGO PRADO DE JESUS <rodrigo.p.jesus@caixa.gov.br>
2018-02-26 11:31:21 -0300

    pequenas correções

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 @echo off
+python download_ajustes_precos_diesel_e_gasolina.py %*
 python download_precos_medios_diesel_e_gasolina.py %*
 python atualiza_base_ajuste_precos_diesel_gasolina.py %*
-python calcula_preco_base_100 %*
+python calcula_preco_base_100.py %*
 pause
