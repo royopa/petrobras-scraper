5f0b7f4be378a4769fc4e0c84b22028ab5974ae8
Rodrigo Prado de Jesus <royopa@gmail.com>
2018-04-10 08:52:22 -0300

    corrigido start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python download_ajustes_precos_diesel_e_gasolina.py %*
-python download_precos_medios_diesel_e_gasolina.py %*
-python atualiza_base_ajuste_precos_diesel_gasolina.py %*
-python calcula_preco_base_100.py %*
+python download_ajustes_precos_diesel_e_gasolina.py
+python download_precos_medios_diesel_e_gasolina.py
+python atualiza_base_ajuste_precos_diesel_gasolina.py
+python calcula_preco_base_100.py
